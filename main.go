3c3519dd0361f0e7a7c030d646b319c227eccaac
sergystepanov <sergystepanov@gmail.com>
2020-10-22 22:17:38 +0300

    Add CI runner

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/veandco/go-sdl2/sdl"
+	"time"
 )
 
 func main() {
@@ -11,8 +11,8 @@ func main() {
 	var winWidth, winHeight int32 = 160, 120
 	var window *sdl.Window
 	var context sdl.GLContext
-	var event sdl.Event
-	var running bool
+	//var event sdl.Event
+	//var running bool
 	var err error
 
 	if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
@@ -41,33 +41,34 @@ func main() {
 	}
 
 	gl.Enable(gl.DEPTH_TEST)
-	gl.ClearColor(0.2, 0.2, 0.3, 1.0)
+	gl.ClearColor(0, 0, 0, 1.0)
 	gl.ClearDepth(1)
 	gl.DepthFunc(gl.LEQUAL)
 	gl.Viewport(0, 0, winWidth, winHeight)
 
-	running = true
-	for running {
-		for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch t := event.(type) {
-			case *sdl.QuitEvent:
-				running = false
-			case *sdl.MouseMotionEvent:
-				fmt.Printf("[%d ms] MouseMotion\tid:%d\tx:%d\ty:%d\txrel:%d\tyrel:%d\n", t.Timestamp, t.Which, t.X, t.Y, t.XRel, t.YRel)
-			}
-		}
+	//running = true
+	//for running {
+	//	for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+	//		switch t := event.(type) {
+	//		case *sdl.QuitEvent:
+	//			running = false
+	//		case *sdl.MouseMotionEvent:
+	//			fmt.Printf("[%d ms] MouseMotion\tid:%d\tx:%d\ty:%d\txrel:%d\tyrel:%d\n", t.Timestamp, t.Which, t.X, t.Y, t.XRel, t.YRel)
+	//		}
+	//	}
 
-		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-		gl.Begin(gl.TRIANGLES)
-		gl.Color3f(1.0, 0.0, 0.0)
-		gl.Vertex2f(0.5, 0.0)
-		gl.Color3f(0.0, 1.0, 0.0)
-		gl.Vertex2f(-0.5, -0.5)
-		gl.Color3f(0.0, 0.0, 1.0)
-		gl.Vertex2f(-0.5, 0.5)
-		gl.End()
+	gl.Begin(gl.TRIANGLES)
+	gl.Color3f(1.0, 0.0, 0.0)
+	gl.Vertex2f(0.5, 0.0)
+	gl.Color3f(0.0, 1.0, 0.0)
+	gl.Vertex2f(-0.5, -0.5)
+	gl.Color3f(0.0, 0.0, 1.0)
+	gl.Vertex2f(-0.5, 0.5)
+	gl.End()
 
-		window.GLSwap()
-	}
+	window.GLSwap()
+	time.Sleep(3 * time.Second)
+	//}
 }
