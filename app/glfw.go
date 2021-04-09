df286c58cd2ef9437129cc7c7abd63f98e0cf2eb
sergystepanov <sergystepanov@gmail.com>
2021-04-09 14:47:20 +0300

    Test GLFW

--- app/glfw.go
+++ app/glfw.go
@@ -33,7 +33,7 @@ func MainGLFW() {
 		//glfw.WindowHint(glfw.BlueBits, 8)
 		glfw.WindowHint(glfw.ContextVersionMajor, 3)
 		glfw.WindowHint(glfw.ContextVersionMinor, 2)
-		//glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+		glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 		glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 	}
 
